1e072fb33d615473d6e94a4f61e472bbb5afd30d
Nadia Sanchez <nsanchez3355@gmail.com>
2024-05-28 23:35:24 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+//ESCRIBIR MIS FUNCIONES
+//FUNCIÓN 1. GENERAR UN COLOR ALEATORIO
+function generaColor(){
+
+}
+
+//Genera mi valor de RGB por separado y guardalos en variables
+const rojo = ;
+const verde = ;
+const azul = ;
+
+// --> RBG(X, Y, Z)
+
+//FUNCIÓN 2. CAMBIAR EL COLOR DE FONDO DEL CUERPO DE LA PÁGINA
+function cambioBackground(){
+
+}
+// Buscar el botón en mi HTML por su clase
+// Cada vez que mi botón sea accionando, voy a llamar o ejecutar mi función cambioBackground;
+
+
